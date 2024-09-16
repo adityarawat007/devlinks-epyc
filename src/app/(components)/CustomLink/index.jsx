35f1bdfa14fb3e4e0518c22be4dcfaf3c6ca89aa
AdityaMakerStudio <adityarawat@makerstudio.io>
2024-09-16 20:48:21 +0530

    feat: implement custom link tab structure

--- src/app/(components)/CustomLink/index.jsx
+++ src/app/(components)/CustomLink/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "../../page.module.css";
 import Button from "../Button";
+import Image from "next/image";
 const CustomLink = () => {
   return (
     <div className={styles.customTab}>
@@ -12,6 +13,19 @@ const CustomLink = () => {
         </p>
       </div>
       <Button title="+ Add New Link" />
+      <div className={styles.addLinkCta}>
+        <Image src="images/illustration-empty.svg" width={250} height={160}  />
+        <h2>Let’s get you started</h2>
+        <p>Use the “Add new link” button to get started. Once you have more than one link, you can reorder and edit them. We’re here to help you share your profiles with everyone!</p>
+      </div>
+      <div
+      style={{
+        justifyContent: "flex-end",
+      }}>
+        <Button style={{
+            backgroundColor: "#633cff",
+        }} title="Save" />
+      </div>
     </div>
   );
 };
